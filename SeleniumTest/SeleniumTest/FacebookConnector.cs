44e74edfe6414501a81cd03f9ee68a7980c6c1e6
Munteanu Silviu <munteanusilviu1996@yahoo.com>
2024-05-28 07:34:50 +0300

    Initial commit for SeleniumConnector

---
+++ SeleniumTest/SeleniumTest/FacebookConnector.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using NUnit.Framework;
+
+namespace SeleniumTest
+{
+    class FacebookConnector
+    {
+        IWebDriver driver = new ChromeDriver();
+        [SetUp]
+        public void Initialize()
+        {
+
+            //navigate to URL  
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            
+            //Maximize the browser window  
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+        }
+        [Test]
+        public void ExecuteTest()
+        {
+            //perform browser operations  
+            IWebElement ele = driver.FindElement(By.Id("email"));
+            ele.SendKeys("abcdef@gmail.com");
+            Thread.Sleep(2000);
+            Console.WriteLine("username value is entered \n");
+            IWebElement ele1 = driver.FindElement(By.Name("pass"));
+            ele1.SendKeys("######");
+            Console.WriteLine("password is entered");
+            IWebElement ele2 = driver.FindElement(By.Id("u_0_5_Ju"));
+            ele2.Click();
+            Thread.Sleep(3000);
+            Console.WriteLine("login button is clicked");
+        }
+        [TearDown]
+        public void EndTest()
+        {
+            //close the browser  
+            driver.Close();
+        }
+    }
+}
---
+++ SeleniumTest/SeleniumTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SeleniumTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IWebDriver driver = new ChromeDriver();
+            //driver.Navigate().GoToUrl("http://www.google.com");
+
+            //IWebElement elementSearch = driver.FindElement(By.Name("q"));
+            //Thread.Sleep(3000);
+            //elementSearch.SendKeys("buy house in Alicante,Spain");
+            //Thread.Sleep(2000);
+
+            //IWebElement elementSearch1 = driver.FindElement(By.Name("btnK"));
+            //elementSearch1.Click();
+
+            //Thread.Sleep(3000);
+            ////close the browser  
+            //driver.Close();
+            //Console.Write("test case ended ");
+
+            FacebookConnector test = new FacebookConnector();
+
+            test.Initialize();
+            test.ExecuteTest();
+            test.EndTest();
+
+        }
+    }
+}
